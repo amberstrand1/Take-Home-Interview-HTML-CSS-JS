8daa8bda0ccf05512846f21f3ccdfb3b3e9f16f6
amberspake1 <amberspake1@gmail.com>
2026-01-20 11:20:58 -0800

    updates for user card

---
+++ user.js
@@ -0,0 +1,29 @@
+const postListEl = document.querySelector('post-list');
+const id = localStorage.getItem("id");
+
+async function onSearchChange(event) {
+    const id = event.target.value;
+    renderPosts(id)
+}
+
+
+async function renderPosts(userId) {
+    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:${userId || id}`)
+    const postsData = await posts.json();
+    postListEl.innerHtml = postsData.map(post => postHtml(post)).join('');
+}
+
+function postHtml(post) {
+    return  `
+        <div class="post">
+            <div class="post__title">
+             ${post.title}
+            </div>
+            <p class="post__body">
+            ${post.body}
+        </p>
+    </div>
+        `
+}
+
+renderPosts(id);
